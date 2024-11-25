9529c939c3b7e3fda74f54e0242a92310e724ca7
Aysuka Ansari <aysukaansari@apps.ipb.ac.id>
2024-11-25 17:00:28 +0700

    added new features i.e lines and bytes counter

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 
 # Word Counter CLI App
-
-A simple app that can count the number of words from STDIN. This App can also be used to count the number of lines and bytes from STDIN.
-
+A simple app that can count the number of words from STDIN. This App can also be used to count the number of lines and bytes from STDIN.  
+## How to install
+### Clone the repository
+        git clone https://github.com/dummyheaad/go-cli-word-counter.git
+### Build the executable
+        go build
+## Examples
+### Word Counter
+        echo "lorem ipsum" | ./wc
+        Output: 2
+### Line Counter
+        cat<< EOF | ./wc -l
+        lorem
+        ipsum
+        sit
+        EOF
+        Output: 3
+### Byte Counter
+        echo -n "lorem" | ./wc -b
+        Output: 5
--- main.go
+++ main.go
@@ -8,15 +8,18 @@ import (
 	"os"
 )
 
-func count(r io.Reader, countLines bool) int {
+func count(r io.Reader, countLines bool, countBytes bool) int {
 	// A scanner is used to read text from a Reader (such as files)
 	scanner := bufio.NewScanner(r)
 
-	// If the count lines flag is not set, we want to count words so we
-	// define the scanner split type to words (default is split by lines)
-	if !countLines {
+	if countBytes {
+		scanner.Split(bufio.ScanBytes)
+	} else if countLines {
+		scanner.Split(bufio.ScanLines)
+	} else {
 		scanner.Split(bufio.ScanWords)
 	}
+	
 
 	// Defining a counter
 	wc := 0
@@ -33,10 +36,11 @@ func count(r io.Reader, countLines bool) int {
 func main() {
 	// Defining a boolean flag -l to count lines instead of words
 	lines := flag.Bool("l", false, "Count lines")
+	bytes := flag.Bool("b", false, "Count bytes")
 	// Parsing the flags provided by the user
 	flag.Parse()
 
 	// Calling the count function to count the number of words
 	// received from the Standard Input and printing it out
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines, *bytes))
 }
